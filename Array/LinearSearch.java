6ed5c825188f196b641464a6acefbe163059ee00
tripurari2004 <tripurarikumar18feb@gmail.com>
2025-06-04 16:28:09 +0530

    Initial Commit

---
+++ Array/LinearSearch.java
@@ -0,0 +1,25 @@
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
+
+        int result = linearSearch(numbers, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
